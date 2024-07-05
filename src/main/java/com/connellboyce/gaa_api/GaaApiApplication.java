e2a4ca9796c06ae8ff1b179e2e065d524986acfe
Connell Boyce <connellboyce17@gmail.com>
2024-07-05 12:17:16 -0400

    feat: Initialize Spring Boot

---
+++ src/main/java/com/connellboyce/gaa_api/GaaApiApplication.java
@@ -0,0 +1,13 @@
+package com.connellboyce.gaa_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaaApiApplication.class, args);
+	}
+
+}
